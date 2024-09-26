905a77dde4678102b16c06c9b6c40232ef916aa9
JB <sebastian03741@gmail.com>
2024-09-26 11:24:36 -0500

    .

--- funciones.js
+++ funciones.js
@@ -3,11 +3,22 @@ function miFuncion(a,b)
     console.log("Suma: "+(a+b))
 }
 
-function suma(num1=0,num2=2)
+function suma(num1,num2)
 {
     let resultado;
     resultado= num1+num2;
     return resultado;
 }
-miFuncion()
-console.log(suma())
+miFuncion(5,3)
+console.log(suma(2,4))
+let x = function (a,b){return a+b};
+let resultado=x(1,2);
+
+const factorial= function fac(n)
+{
+    return n<2 ? 1 : n*fac(n-1);
+};
+console.log(factorial(3));
+const sumarFuncionFlecha =(nu1,nu2) => nu1+nu2;
+resultado1= sumarFuncionFlecha(3,4)
+console.log(resultado1)
